cb072502137b259f214c5debb89b24d11da5cf94
Stephen Rushe <srushe@users.noreply.github.com>
2018-10-24 17:32:41 +0100

    Remove deprecation warning
    `Parser::Rewriter` is [deprecated](https://github.com/whitequark/parser/blob/fbe0e8cbec557c96b0e0f4a8c5201155ee478284/lib/parser/rewriter.rb#L6), so we should update to `Parser::TreeRewriter`.

--- lib/rspectre/auto_corrector.rb
+++ lib/rspectre/auto_corrector.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module RSpectre
-  class AutoCorrector < Parser::Rewriter
+  class AutoCorrector < Parser::TreeRewriter
     include Concord.new(:filename, :nodes, :buffer)
 
     def initialize(filename, nodes)
